471cce51a032413eb7fc4e4a1c0662a62110e2f1
Alex Abreu <alexsantosdeabreu@gmail.com>
2020-08-06 09:25:31 -0300

    Adding vehicles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,83 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Vehicles = Vehicles.create([
+                               {
+                                   brand: "Chevrolet",
+                                   vehicle: "Chevrolet Equinox",
+                                   year: 2019,
+                                   description: "2.0 16V TURBO GASOLINA PREMIER AWD AUTOMÁTICO",
+                                   sold: false
+                               },
+                               {
+                                   brand: "Chevrolet",
+                                   vehicle: "Chevrolet Equinox",
+                                   year: 2019,
+                                   description: "2.0 16V TURBO GASOLINA PREMIER AWD AUTOMÁTICO",
+                                   sold: true
+                               },
+                               {
+                                   brand: "Chevrolet",
+                                   vehicle: "Chevrolet Colbat",
+                                   year: 2014,
+                                   description: "1.8 MPFI LTZ 8V FLEX 4P MANUAL",
+                                   sold: false
+                               },
+                               {
+                                   brand: "Chevrolet",
+                                   vehicle: "Chevrolet Cruze",
+                                   year: 2019,
+                                   description: "1.4 TURBO LTZ 16V FLEX 4P AUTOMÁTICO",
+                                   sold: false
+                               },
+                               {
+                                   brand: "Chevrolet",
+                                   vehicle: "Chevrolet Corsa",
+                                   year: 1999,
+                                   description: "1.0 MPF WIND 8V GASOLINA 2P MANUAL",
+                                   sold: false
+                               },
+                               {
+                                   brand: "Fiat",
+                                   vehicle: "Fiat Punto",
+                                   year: 2009,
+                                   description: "1.8 HLX 8V FLEX 4P MANUAL",
+                                   sold: false
+                               },
+                               {
+                                   brand: "Fiat",
+                                   vehicle: "Fiat Palio",
+                                   year: 2008,
+                                   description: "1.4 MPI ELX 8V FLEX 4P MANUAL",
+                                   sold: false
+                               },
+                               {
+                                   brand: "Fiat",
+                                   vehicle: "Fiat Toro",
+                                   year: 2018,
+                                   description: "1.8 16V EVO FLEX ENDURANCE AT6",
+                                   sold: false
+                               },
+                               {
+                                   brand: "Fiat",
+                                   vehicle: "Fiat Toro",
+                                   year: 2019,
+                                   description: "1.8 16V EVO FLEX ENDURANCE AT6",
+                                   sold: true
+                               },
+                               {
+                                   brand: "Fiat",
+                                   vehicle: "Fiat Linea",
+                                   year: 2014,
+                                   description: "1.8 ABSOLUTE 16V FLEX 4P AUTOMATIZADO",
+                                   sold: false
+                               },
+                               {
+                                   brand: "Ford",
+                                   vehicle: "Ford KA",
+                                   year: 2018,
+                                   description: "1.0 TI-VCT FLEX SE MANUAL",
+                                   sold: false
+                               }
+                           ])
